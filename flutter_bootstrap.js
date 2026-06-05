598542a8244a5edf982507c847ce4874fe9b56c7
Mesta19 <officialbadkick@gmail.com>
2026-06-05 13:39:00 +0700

    Fix: home_screen import notification_helper agar web popup aktif

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "939660863" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3470771866" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
